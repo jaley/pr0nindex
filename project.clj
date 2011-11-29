057ffb84496d72d4e9b05ff92c24007d88bf3274
James Aley <james.aley@gmail.com>
2011-11-29 10:44:18 +0000

    Trimming down ring dependencies.

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "GAE demo of the pr0n index."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [ring/ring-core "0.3.5"]
+                 [ring/ring-servlet "0.3.5"]
                  [compojure "0.5.3"]
-                 [ring "0.3.11"]
-                 [com.google.appengine/appengine-tools-sdk "1.6.0"]
-                 [clj-json "0.3.1"]]
+                 [clj-json "0.3.1"]
+                 [com.google.appengine/appengine-tools-sdk "1.6.0"]]
   :compile-path "war/WEB-INF/classes/"
   :library-path "war/WEB-INF/lib/"
   :aot [pr0nindex.core])
