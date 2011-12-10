3376cce1146cc3d8c300082e6526770e1ce40b38
James Aley <james.aley@gmail.com>
2011-12-10 16:01:31 +0000

    Adding a progress bar for pr0n ratio.

--- war/js/pr0n.js
+++ war/js/pr0n.js
@@ -1,19 +1,39 @@
+MAX_PENIS_LENGTH = 25.0;
+
+/**
+ * Add a repeat function to String
+ */
+String.prototype.repeat = function(n) {
+    return new Array(n+1).join(this);
+}
+
+/**
+ * Guess.
+ */
+function makePenis(length) {
+    return "8" + "=".repeat(length) + "D";
+}
+
+/**
+ * Make the summary text for the results.
+ */
+function makeLabel(phrase, ratio) {
+    return "The phrase \"" + phrase + "\"" +
+        " is " + (ratio * 100).toFixed(2) + "% pr0n!";
+}
+
 /**
  * Do ajax call for pr0n check and write result to resElem.
  */
-function doSearch(inputElem, resElem) {
-    resElem.empty();
-    
+function doSearch(inputElem, gauge, label) {
     var p = inputElem.val();
     $.ajax({
         url: '/pindex',
         dataType: 'json',
         data: {p: p},
         success: function(data) {
-            $(resElem)
-                .append('<span/>')
-                .addClass('result')
-                .text(data.phrase + ", " + data.ratio);
+            $(gauge).text(makePenis(Math.round(data.ratio * MAX_PENIS_LENGTH)));
+            $(label).text(makeLabel(data.phrase, Math.round(data.ratio)));
         }
     });
 }
@@ -22,10 +42,11 @@ function doSearch(inputElem, resElem) {
 $(function() {
     var searchField = $("#search_field");
     var phraseField = $("#phrase_field");
-    var resElem = $("#results");
+    var gauge = $("#willy");
+    var label = $("#text_results");
 
     searchField.submit(function() {
-        doSearch(phraseField, resElem);
+        doSearch(phraseField, gauge, label);
         return false;
     });
 });
