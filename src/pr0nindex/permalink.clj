a2d3962304102c42fbbf7ea4506b325df043a898
James Aley <james.aley@gmail.com>
2011-12-11 13:45:02 +0000

    Missed on last commit.

---
+++ src/pr0nindex/permalink.clj
@@ -0,0 +1,24 @@
+(ns pr0nindex.permalink
+  (:use net.cgrand.enlive-html))
+
+(def *max-penis-length* 25.0)
+
+(defn make-penis
+  [length]
+  (str "8=" (apply str (take (int length) (repeat "="))) "D"))
+
+(defn make-label
+  [phrase index]
+  (format "The phrase \"%s\" is %1.2f%% pr0n!" phrase index))
+
+(deftemplate permalink-template "templates/perm-template.html"
+  [label penis]
+  [:#willy] (content penis)
+  [:#text_results] (content label))
+
+(defn page
+  "Render a permalink page for the given phrase and pr0n index."
+  [phrase index]
+  (apply str (permalink-template
+              (make-label phrase index)
+              (make-penis (* index *max-penis-length*)))))
