a28c4dbb2a6c9f3b1d7ab83347b76f7cab305dc8
James Aley <james.aley@gmail.com>
2011-12-11 18:32:25 +0000

    Changing to using permalink all the time.

--- src/pr0nindex/permalink.clj
+++ src/pr0nindex/permalink.clj
@@ -1,5 +1,6 @@
 (ns pr0nindex.permalink
-  (:use net.cgrand.enlive-html))
+  (:use net.cgrand.enlive-html)
+  (:import java.net.URLEncoder))
 
 (def *max-penis-length* 25.0)
 
@@ -12,13 +13,16 @@
   (format "The phrase \"%s\" is %1.2f%% pr0n!" phrase (* 100 index)))
 
 (deftemplate permalink-template "templates/perm-template.html"
-  [label penis]
+  [phrase label penis]
   [:#willy] (content penis)
-  [:#text_results] (content label))
+  [:#text_results] (content label)
+  [:#tweet_button] (set-attr :data-url
+                             (str "http://pr0nindex.appspot.com/perma?p=" (URLEncoder/encode phrase))))
 
 (defn page
   "Render a permalink page for the given phrase and pr0n index."
   [phrase index]
   (apply str (permalink-template
+              phrase
               (make-label phrase index)
               (make-penis (* index *max-penis-length*)))))
