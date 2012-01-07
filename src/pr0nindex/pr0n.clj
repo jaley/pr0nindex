6788c521195b405026d1b1280f03b8da04dc77a0
James Aley <james.aley@gmail.com>
2012-01-07 17:49:15 +0000

    Improved results accuracy by switching to moderate for filtered results when phrase contains profanities.

--- src/pr0nindex/pr0n.clj
+++ src/pr0nindex/pr0n.clj
@@ -1,7 +1,9 @@
 (ns pr0nindex.pr0n
   (:use pr0nindex.key
+        pr0nindex.profanities
         clojure.contrib.json)
-  (:require [clojure.contrib.duck-streams :as duck])
+  (:require [clojure.contrib.duck-streams :as duck]
+            [clojure.string :as string])
   (:import [java.net URL URLEncoder]))
 
 (def *url-format*
@@ -9,12 +11,12 @@
 
 (defn make-url
   "Construct a java.net.URL for the search request."
-  [phrase safe?]
+  [phrase safe]
   (URL.
    (format *url-format*
            (URLEncoder/encode (str \" phrase \") "UTF-8")
            *api-key*
-           ({true "active", false "off"} safe?))))
+           safe)))
 
 (defn search!
   "Executes query URL and returns results from JSON parser."
@@ -31,14 +33,21 @@
 
 (def clamp (comp (partial min 1.0) (partial max 0.0)))
 
+(defn safe-type
+  "If phrase contains profanities, use moderate, else high."
+  [phrase]
+  (if (some profanity-set (string/split phrase #"\s+"))
+    "moderate"
+    "high"))
+
 (defn pr0n-index*
   "Return a value between 0 and 1, where 0 indicates unlikely offensive material,
    and 1 is almost certainly offensive."
   [phrase]
   (if (nil? phrase)
     0.0 ; guard for nil
-    (let [hits_on  (hits (search! (make-url phrase true)))
-          hits_off (hits (search! (make-url phrase false)))]
+    (let [hits_on  (hits (search! (make-url phrase (safe-type phrase))))
+          hits_off (hits (search! (make-url phrase "off")))]
       (if (> hits_off 0)
         (clamp (/ (- hits_off hits_on) hits_off))
         0.0))))
--- test/pr0nindex/test/pr0n.clj
+++ test/pr0nindex/test/pr0n.clj
@@ -3,29 +3,35 @@
   (:use [clojure.test]))
 
 (defn pr0n?
+  "Return true if a phrase is more than 80% pr0n."
   [phrase]
   (> (pr0n-index phrase) 0.8))
-(def npr0n? (complement pr0n?))
+
+(defn safe?
+  "Return true if a phrase is less than 50% pr0n, thus likely safe, maybe."
+  [phrase]
+  (<= (pr0n-index phrase) 0.5))
 
 (deftest test-pr0n-index-positive
   (is (pr0n? "man juice"))
   (is (pr0n? "hot mom"))
   (is (pr0n? "lex steele"))
-  (is (pr0n? "huge jugs")))
+  (is (pr0n? "huge jugs"))
+  (is (pr0n? "twat pounding")))
 
 (deftest test-pr0n-index-negative
-  (is (npr0n? "man"))
-  (is (npr0n? "juice"))
-  (is (npr0n? "hot"))
-  (is (npr0n? "mom"))
-  (is (npr0n? "lex"))
-  (is (npr0n? "steele"))
-  (is (npr0n? "huge"))
-  (is (npr0n? "jugs")))
+  (is (safe? "man"))
+  (is (safe? "juice"))
+  (is (safe? "hot"))
+  (is (safe? "mom"))
+  (is (safe? "lex"))
+  (is (safe? "steele"))
+  (is (safe? "huge"))
+  (is (safe? "jugs")))
 
 (deftest test-pr0n-index-garbage
-  (is (npr0n? nil))
-  (is (npr0n? ""))
-  (is (npr0n? "123 5432 1234 1"))
-  (is (npr0n? "!!223 __ 123431")))
+  (is (safe? nil))
+  (is (safe? ""))
+  (is (safe? "123 5432 1234 1"))
+  (is (safe? "!!223 __ 123431")))
 
